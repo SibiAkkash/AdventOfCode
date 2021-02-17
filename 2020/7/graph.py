eb510827c2bab8341d67e2949ef6c4c15cc39dc1
Sibi Akkash <sibi.akkash@gmail.com>
2021-02-17 11:14:26 +0530

    day 7

---
+++ 2020/7/graph.py
@@ -0,0 +1,70 @@
+
+
+from typing import List, Tuple
+Node = str
+Weight = int
+
+
+class Graph:
+	def __init__(self, num_vertices: int) -> None:
+		# stores bag_types
+		self.vertices = []
+		# maps bag_type to an id
+		self.vertex_map = {}
+		# adjacency matrix
+		self.adj = [[0 for i in range(num_vertices)] for j in range(num_vertices)]
+
+	def add_vertex(self, bag_type: str) -> None:
+		# self.vertices = ['blue', 'black', 'red', ....]
+		# self.vertex_map[2] = "red"
+		# self.adj[2][3]: edge from bag 2 -> bag 3, with a weight
+		self.vertices.append(bag_type)
+		self.vertex_map[bag_type] = len(self.vertices) - 1
+
+	# adds an directed weighted edge from outer_bag to inner_bag with weight = count
+	def add_edge(self, outer_bag: str, inner_bag: str, count: int) -> None:
+		# check if vertices are present in graph
+		if outer_bag not in self.vertices:
+			self.add_vertex(outer_bag)
+		if inner_bag not in self.vertices:
+			self.add_vertex(inner_bag)
+
+		outer_bag_id = self.vertex_map[outer_bag]
+		inner_bag_id = self.vertex_map[inner_bag]
+		self.adj[outer_bag_id][inner_bag_id] = count
+
+	def get_vertices(self) -> List[str]:
+		return self.vertices
+	
+	def get_adjacency_matrix(self) -> List[List[int]]:
+		return self.adj	
+
+	def show_adjacency_matrix(self) -> None:
+		for i in range(len(self.adj)):
+			print(self.vertices[i], end=' ->\t')
+			print(self.adj[i])
+
+	def get_incoming_vertices(self, bag_type: Node) -> List[Node]:
+		vertices = []
+		bag_type_id = self.vertex_map[bag_type]
+		# print(f'{bag_type} id: {bag_type_id}')
+		for i in range(len(self.adj)):
+			# if there is an edge from some bag to the given gold bag
+			# add it to the vertices []
+			if self.adj[i][bag_type_id] != 0:
+				vertices.append(self.vertices[i])
+
+		return vertices
+
+	def get_adjacent_vertices(self, bag_type: Node) -> List[Tuple[Node, Weight]]:
+		vertices = []
+		bag_type_id = self.vertex_map[bag_type]
+		for i in range(len(self.adj)):
+			bag_count = self.adj[bag_type_id][i]
+			inner_bag_type = self.vertices[i]
+			if bag_count != 0:
+				vertices.append((inner_bag_type, bag_count))
+		return vertices
+
+	
+	
--- 2020/7/solution.py
+++ 2020/7/solution.py
@@ -1,24 +1,13 @@
-
-
-''' 
-	light red -> 1 bright white, 2 muted yellow
-	dark orange -> 3 bright white, 4 muted yellow
-	bright white -> 1 shiny gold
-	muted yellow -> 2 shiny gold, 9 faded blue
-	shiny gold -> 1 dark olive, 2 vibrant plum
-	dark olive -> 3 faded blue, 4, dotted black
-	vibrant plum -> 5 faded blue, 6 dotted black
-	faded blue -> none
-	dotted black -> none
-'''
-
 from typing import List, Dict, Tuple
+from graph import Graph
+from collections import deque
 
 Node = str
 Weight = int
 ParsedRule = List[Tuple[Node, Node, Weight]]
 
 def parse_rule(rule: str) -> ParsedRule:
+	# sample rule: shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
 	parts = rule.split("bags contain")
 	
 	# get outer and inner bags for graph
@@ -45,23 +34,79 @@ def parse_rule(rule: str) -> ParsedRule:
 	return inner_bags	
 
 
-def make_graph(rules: List[str]) -> Dict[str, int]:
-	nodes = []
-	n = len(nodes)
-	# graph = [[0 for i in range(n)] for j in range(n)]
-	for rule in rules:
-		print(rule, parse_rule(rule))
-
+def make_graph(rules: List[str]) -> Graph:
 	# each parsed rule is a list of edges of a directed weighted graph
 	# add each edge (rule) to the graph, build the graph from the edges
 	# the problem is to find the number of bag colours that can eventually contain a shiny gold bag
 	# which is to ask how many nodes in the graph have a path to the target node (shiny gold bag) 
+	bag_types = set()
+	parsed_rules = []
+	for rule in rules:
+		edges = parse_rule(rule)
+		for edge in edges:
+			# print(edge)
+			bag_type_1, bag_type_2 = edge[0], edge[1]
+			# add the different bag_types to a set
+			bag_types.add(bag_type_1)
+			# this could be and edge that goes no where, that is this bag_type cannot contain any bags
+			if bag_type_2 is not None:
+				parsed_rules.append(edge)
+				bag_types.add(bag_type_2)
+			
+	#* we've got all the bag types as the nodes of a graph
+	#*  and the rules in the form of edges of a graph
+	# print(bag_types)
+	# print(*parsed_rules, sep='\n')
 
+	# add the nodes and edges to a graph
+	graph = Graph(len(bag_types))
+	for bag_type in bag_types:
+		graph.add_vertex(bag_type)
+	for rule in parsed_rules:
+		graph.add_edge(rule[0], rule[1], int(rule[2]))
+	
+	return graph
 
+def bags_that_can_contain(graph: Graph, bag_type: str) -> int:
+	visited = set()
+	# get bags that directly can contain the shiny gold bag
+	vertices_list = graph.get_incoming_vertices('shiny gold')
+	# make it a deque for faster pops from front
+	current_bags = deque(bag for bag in vertices_list)
+	# get all bags that can eventually contain the gold bag
+	while current_bags:
+		current_bag = current_bags.popleft()
+		if current_bag in visited:
+			continue
+		next_order_bags = graph.get_incoming_vertices(current_bag)
+		current_bags.extend(next_order_bags)
+		visited.add(current_bag)
+		
+	# print(visited)
+	return len(visited)
+
+def number_of_bags_inside(graph: Graph, bag_type: str) -> int:
+	if bag_type is None:
+		return 0
+	adj_vertices = graph.get_adjacent_vertices(bag_type)
+	# print(adj_vertices)
+	if len(adj_vertices) == 0:
+		return number_of_bags_inside(graph, None)
+	total = 0
+	for inner_bag in adj_vertices:
+		inner_bag_type, count = inner_bag
+		total += count + count * number_of_bags_inside(graph, inner_bag_type)
+	return total
 
 if __name__ == "__main__":
-	with open('./7/sample_input.txt', 'r') as input_file:
+	input_file = 'input.txt'
+	with open(f'./7/{input_file}', 'r') as input_file:
 		input_text = input_file.read()
 		input = input_text.split('\n')
-	make_graph(input)
+	graph = make_graph(input)
+	# print(f'bag types: {graph.get_vertices()}')
+	# graph.show_adjacency_matrix()
+	# count = bags_that_can_contain(graph, 'shiny gold')
+	# print(count)
+	print(number_of_bags_inside(graph, 'shiny gold'))
 	
